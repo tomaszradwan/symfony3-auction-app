a739b6855c70cea556bb598b1454a474131aad8b
tomaszradwan <radwan.tomasz@gmail.com>
2018-05-03 21:46:38 +0200

    add FOSUserBundle (https://symfony.com/doc/master/bundles/FOSUserBundle/index.html)

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tomasz
+ * Date: 2018-05-03
+ * Time: 21:28
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * User constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
